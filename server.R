45e01b502c4e54cd7ce4a516be66a8fb4b47e6e7
Pradeep K. Pant <ppant@cpan.org>
2016-08-10 14:57:39 +0530

    basic skeleton making

---
+++ server.R
@@ -0,0 +1,24 @@
+
+## Course Project data products Shiny application"
+## author: "Pradeep K. Pant, ppant@cpan.org"
+## date: "Aug 10, 2016"
+## Server side logic (server.R)
+
+## Load Shiny library
+library(shiny)
+
+# Server logic 
+shinyServer(function(input, output) {
+   
+  # output$distPlot <- renderPlot({
+  #   
+  #   # generate bins based on input$bins from ui.R
+  #   x    <- faithful[, 2] 
+  #   bins <- seq(min(x), max(x), length.out = input$bins + 1)
+  #   
+  #   # draw the histogram with the specified number of bins
+  #   hist(x, breaks = bins, col = 'darkgray', border = 'white')
+  #   
+  # })
+  
+})
---
+++ ui.R
@@ -0,0 +1,29 @@
+
+## Course Project data products Shiny application"
+## Author: "Pradeep K. Pant, ppant@cpan.org"
+## date: "Aug 10, 2016"
+## Build User interface (ui.R)
+
+## Load Shiny library
+library(shiny)
+
+# Define UI for the application 
+shinyUI(fluidPage(
+  
+  # Application title
+  # titlePanel("Prediction analysis app"),
+  
+  # # Sidebar with a slider input for number of bins 
+  # sidebarLayout(
+  #   sidebarPanel(
+  #      sliderInput("bins",
+  #                  "Number of bins:",
+  #                  min = 1,
+  #                  max = 50,
+  #                  value = 30)
+  #   ),
+  #   
+  #   # Show a plot of the generated distribution
+  #   mainPanel(
+  #      plotOutput("distPlot")
+    ))
