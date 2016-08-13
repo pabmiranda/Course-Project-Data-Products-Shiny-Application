ab30753f27cca59ff2c60e0e993ee02aafd5e8b2
Pradeep K. Pant <ppant@cpan.org>
2016-08-13 23:45:23 +0530

    added reactive logic and sample grapj

--- server.R
+++ server.R
@@ -2,19 +2,32 @@
 ## Course Project data products Shiny application"
 ## author: "Pradeep K. Pant, ppant@cpan.org"
 ## date: "Aug 10, 2016"
-## Server side logic (server.R) to build the appx
+## Server side logic (server.R) to implement event fired by UI
 
 ## Load Shiny library
 library(shiny)
 library(datasets)
 library(dplyr)
+library(ggplot2)
 
 # Server logic 
 shinyServer(function(input, output) {
   
   output$Details <- renderDataTable({
-    data <- transmute(WaterQualityRiverGanga2012, PH = V12, Temperature = V4,
-                      Conductivity = V15, Coliform = V26, Nitrate = V20)
+    output$text1 <- renderText({ 
+      # paste("You have selected", input$var)
+    })
+    WaterQualityRiverGanga2012 <- filter(WaterQualityRiverGanga2012, V3 == input$var)
+    
+    data <- transmute(WaterQualityRiverGanga2012, PH = V12, Temperature = V4, Conductivity = V15, Coliform = V26, Nitrate = V20)
     data
   }, options = list(lengthMenu = c(5, 10, 20), pageLength = 10))
+  output$plot1 <- renderPlot({
+   # plot(WaterQualityRiverGanga2012$V3)
+  #hist(GangaRiverWaterAnalysis2012)
+    p<-qplot(WaterQualityRiverGanga2012$V3,WaterQualityRiverGanga2012$V26, data=WaterQualityRiverGanga2012, color=WaterQualityRiverGanga2012$V3, main = "State wise coliform bacteria in water sample",xlab = "State name",ylab="Coliform Value")
+    p + theme_bw()
+    p + theme(axis.title=element_text(face="bold.italic", size="12", color="brown"), legend.position="top") 
+    p + guides(fill=guide_legend(title="State wise coliform value"))
+  })
 })
--- ui.R
+++ ui.R
@@ -13,19 +13,22 @@ shinyUI(fluidPage(
   titlePanel("Water Quality of River Ganga (India) in various places on-route (Year 2012)"),
   br(),
   br(),
-    sidebarPanel( 
-      selectInput("select", label = h3("Choose state(s)"), 
-                               choices = list("UTTARANCHAL" = 1, 
-                                              "UTTAR PRADESH" = 2, 
-                                              "BIHAR" = 3,
-                                              "WEST BENGAL"= 4),
-                               selected = 1),
-      
-      selectInput("select", label = h3("Select box"), 
-                       choices = list("Choice 1" = 1, "Choice 2" = 2,
-                                      "Choice 3" = 3), selected = 1)
-    ),
   
-mainPanel(dataTableOutput('Details'))
+
+    sidebarPanel(
+      helpText("Analyse Water Quality of River Ganga (State wise)"),
+      selectInput("var", 
+                  label = "Choose an state",
+                  choices = c("UTTARANCHAL", "UTTAR PRADESH",
+                              "BIHAR", "WEST BENGAL"),
+                  selected = "UTTARANCHAL")),
+      br(),
+      br(),
+    
+mainPanel(dataTableOutput('Details'),
+textOutput("text1"),
+br(),
+br(),
+plotOutput("plot1"))
 )
 )
